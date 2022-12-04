86dc52ff97224083f4d20ca8a41e3f8974416899
Taylor Newman <taylor@flightdigital.co.nz>
2022-12-05 09:10:13 +1300

    Created basic repo of issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,32 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+require("dotenv").config({
+  path: `.env.development`,
+});
+const path = require("path");
+
+const isProd = process.env.GATSBY_NODE_ENV === "production";
+const previewEnabled =
+  (process.env.GATSBY_IS_PREVIEW || "false").toLowerCase() === "true";
+
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: process.env.SANITY_PROJECT_ID,
+        dataset: process.env.SANITY_DATASET,
+        // a token with read permissions is required
+        // if you have a private dataset
+        token: process.env.SANITY_TOKEN,
+        watchMode: !isProd, // watchMode only in dev mode
+        overlayDrafts: !isProd || previewEnabled, // drafts in dev & Gatsby Cloud Preview
+      },
+    },
+    `gatsby-plugin-image`,
+  ],
+};
